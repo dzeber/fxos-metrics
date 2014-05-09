5ee1c6465fb4f81180d2b15cd90bf29fd35be671
Dave Zeber <dzeber@mozilla.com>
2014-05-09 18:13:09 -0500

    Added job to count records

---
+++ jobscripts/count_records.py
@@ -0,0 +1,9 @@
+import json
+
+def map(key, dims, value, context):
+    context.write("num_records", 1)
+
+def reduce(key, values, context):
+    context.write(key, sum(values))
+    
+combine = reduce
