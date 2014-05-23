de1abb6f9d648884aa458fd34c8a4bf406282558
Dave Zeber <dzeber@mozilla.com>
2014-05-23 12:37:56 -0500

    Store output in working dir directly

--- runjob.sh
+++ runjob.sh
@@ -6,12 +6,12 @@ BASE=$(pwd)
 THIS_DIR=$(cd "`dirname "$0"`"; pwd)
 TELEMETRY_SERVER_DIR=$HOME/telemetry-server
 
-OUTPUT=${OUTPUT:-output}
+# OUTPUT=${OUTPUT:-output}
 #TODAY=$(date +%Y%m%d)
 
-if [ ! -d "$OUTPUT" ]; then
-    mkdir -p "$OUTPUT"
-fi
+# if [ ! -d "$OUTPUT" ]; then
+    # mkdir -p "$OUTPUT"
+# fi
 
 # if [ ! -d "job" ]; then
     # mkdir -p "job"
@@ -26,8 +26,12 @@ if [ ! -d "data" ]; then
 fi
 
 JOB_FILE=$THIS_DIR/jobscripts/$1
-OUTPUT_FILE=$BASE/$OUTPUT/$2
+# OUTPUT_FILE=$BASE/$OUTPUT/$2
+OUTPUT_FILE=$BASE/$2
 FILTER=$THIS_DIR/filters/${3:-all_fxos.json}  
+
+echo "Running job $JOB_FILE with filter $FILTER"
+echo "Dumping output to $OUTPUT_FILE"
 
 # If we have an argument, process that day.
 # TARGET=$1
@@ -63,3 +67,4 @@ python -m mapreduce.job "$JOB_FILE" \
 
 echo "Mapreduce job exited with code: $?"
 
+echo "Output is located in $OUTPUT_FILE"
