8884cb0e0878200f0a9e2425662c0dbecb99113f
Dave Zeber <dzeber@mozilla.com>
2014-07-30 18:50:44 -0500

    Added skeleton for job to output records as csv

--- jobscripts/dump_csv.py
+++ jobscripts/dump_csv.py
@@ -2,3 +2,15 @@ import json
 
 # Sanitize records and output as CSV.
 
+
+def map(key, dims, value, context):
+    data = json.loads(value)
+    
+ 
+# Set CSV output. 
+def setup_reduce(context):
+    context.field_separator = ','
+
+
+def reduce(key, values, context):
+    context.writecsv(values)
