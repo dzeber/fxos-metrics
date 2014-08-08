c5ead18b8d28c110b529e0d0094f152fcb41843b
Dave Zeber <dzeber@mozilla.com>
2014-08-07 21:35:54 -0500

    Adding runner script to process FTU data into csv

--- processing/generate_csv.sh
+++ processing/generate_csv.sh
@@ -2,12 +2,59 @@
 
 ## Download FTU records, sanitize and output as CSV.
 
-## Download FxOS data scripts. 
-git clone https://github.com/dzeber/fxos-metrics.git
-chmod u+x fxos-metrics/runjob.sh
+DATA_DIR=$HOME/fxos-ftu-data
+SCRIPT_DIR=$HOME/software/fxos-metrics/processing
+LOG_FILE=$DATA_DIR/processing.log
+DATA_FILE=$DATA_DIR/ftu_data.out
+JOB_LOG=$DATA_DIR/ftu_job.log
 
-output=ftu_data.csv
-job=dump_csv.py
 
-fxos-metrics/runjob.sh "$job" "$output"
+CSV_FILE=$DATA_DIR/data.csv
+UPDATED_TIME_FILE=$DATA_DIR/last_updated
+
+
+ADDR=dzeber
+
+exec > $LOG_FILE 2>&1
+
+# Update output files from latest run. 
+echo "Downloading latest output from AWS."
+
+rm $DATA_DIR/*
+aws s3 cp s3://telemetry-private-analysis/fxosping/data/ $DATA_DIR --recursive
+
+if [ ! -e "$DATA_FILE" ]; then
+    # Something went wrong - no data file downloaded.
+    echo "No data file!"
+    # Check for log file. 
+    if [ ! -e "$JOB_LOG" ]; then
+        echo "No log file either!!"
+        cat "-- No log file --" > $JOB_LOG
+    fi
+    # Send email notice with log file as text. 
+    mail -s "FAILED: FxOS FTU data - no data" "$ADDR@mozilla.com" < $JOB_LOG
+    echo "Sent email notice. Exiting..."
+    exit
+fi
+
+# At this point we should have the latest data. 
+echo "Processing data..."
+python generate_csv.py $DATA_FILE $CSV_FILE
+
+if [ ! -e "$CSV_FILE" ]; then
+    echo "Something went wrong - no CSV file generated!"
+    echo "" | mail -s "FAILED: FxOS FTU data - no csv" "$ADDR@mozilla.com" 
+    exit
+fi
+
+# Look up the time the data was updated. 
+echo "Recorded data update time."
+date -d "`stat -c %z $DATA_FILE`" +"%Y-%m-%d %H:%M:%S" > UPDATED_TIME_FILE
+
+# Copy files to web server. 
+echo "Copying data to app1."
+
+
+echo "Done."
+exit 0
 
