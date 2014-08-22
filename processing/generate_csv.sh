200b64b6ff02f8ca68fdf9b34ee66c32d20f5d20
Dave Zeber <dzeber@mozilla.com>
2014-08-21 19:05:20 -0500

    Added code to scp new data to server

--- processing/generate_csv.sh
+++ processing/generate_csv.sh
@@ -8,10 +8,10 @@ LOG_FILE=$DATA_DIR/processing.log
 DATA_FILE=$DATA_DIR/ftu_data.out
 JOB_LOG=$DATA_DIR/ftu_job.log
 
-
-CSV_FILE=$DATA_DIR/data.csv
-UPDATED_TIME_FILE=$DATA_DIR/last_updated
-
+CSV_FILENAME=data.csv
+CSV_PATH=$DATA_DIR/$CSV_FILENAME
+UPDATED_TIME_FILENAME=last_updated
+UPDATED_TIME_PATH=$DATA_DIR/$UPDATED_TIME_FILENAME
 
 ADDR=dzeber
 
@@ -39,9 +39,9 @@ fi
 
 # At this point we should have the latest data. 
 echo "Processing data..."
-python $SCRIPT_DIR/generate_csv.py $DATA_FILE $CSV_FILE
+python $SCRIPT_DIR/generate_csv.py $DATA_FILE $CSV_PATH
     
-if [ ! -e "$CSV_FILE" ]; then
+if [ ! -e "$CSV_PATH" ]; then
     echo "Something went wrong - no CSV file generated!"
     echo "" | mail -s "FAILED: FxOS FTU data - no csv" "$ADDR@mozilla.com" 
     exit
@@ -49,11 +49,25 @@ fi
 
 # Look up the time the data was updated. 
 echo "Recorded data update time."
-date -r $DATA_FILE +"%Y-%m-%d %H:%M:%S" > $UPDATED_TIME_FILE
+date -r $DATA_FILE +"%Y-%m-%d %H:%M:%S" > $UPDATED_TIME_PATH
 
 # Copy files to web server. 
 echo "Copying data to app1."
-
+cd $DATA_DIR
+# Append underscore to existing data files. 
+ssh $APP1 ". .bash_profile;
+    cd \$FTU/data; \
+    rm -f *_ ; \
+    mv $CSV_FILENAME ${CSV_FILENAME}_; \
+    mv $UPDATED_TIME_FILENAME ${UPDATED_TIME_FILENAME}_"
+    
+tar czf new_data $CSV_FILENAME $UPDATED_TIME_FILENAME 
+scp new_data "$APP1:\$HOME"
+ssh $APP1 ". .bash_profile; \
+    tar xzf new_data -C \$FTU/data; \
+    chmod o+r \$FTU/data/$CSV_FILENAME \$FTU/data/$UPDATED_TIME_FILENAME; \
+    rm new_data"
+rm new_data
 
 echo "Done."
 exit 0
