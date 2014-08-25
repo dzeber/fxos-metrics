eed432aad2045df1ecc664ab9ed1336f038313f8
Dave Zeber <dzeber@mozilla.com>
2014-08-25 15:42:37 -0500

    Adding file to package script for running on AWS

---
+++ package-job.sh
@@ -0,0 +1,26 @@
+# Package necessary files to run job on AWS cluster for uploading to S3.
+# Requires filename for tarball as parameter.
+
+THIS_DIR=$(cd "`dirname "$0"`"; pwd)
+PACKAGE_DIR="~/tmp/package"
+TARBALL_NAME=
+
+mkdir -p $PACKAGE_DIR
+cd $PACKAGE_DIR
+
+# Create directory structure. 
+mkdir jobscripts filters lookup
+cp $THIS_DIR/jobscripts/count_activations.py jobscripts
+cp $THIS_DIR/filters/all_fxos.json filters
+cp $THIS_DIR/lookup/* lookup
+cp $THIS_DIR/scheduled_runner.sh .
+# Permissions. 
+chmod -R 755 .
+
+# Package. 
+cd ..
+tar czvf "${1:-fxosping-0.2}.tar.gz" $PACKAGE_DIR/*
+rm -r $PACKAGE_DIR
+
+exit 0
+
