66fa5da967bed24e86816818f1474b033ee4fc71
Dave Zeber <dzeber@mozilla.com>
2014-08-27 12:39:45 -0500

    Updated job script to refer to formmatter module correctly

--- jobscripts/count_activations.py
+++ jobscripts/count_activations.py
@@ -6,13 +6,13 @@ from datetime import date, timedelta
 import copy
 import os.path
 
-import format_field_values
+import ftu_formmatter
 
 
 # The directory containing the lookup tables. 
 lookup_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "lookup")
 
-# Loading for whitelists. 
+# Loading for whitelists    . 
 # Convert each list to convenient format for querying. 
 def load_whitelist():
     with open(os.path.join(lookup_dir, 'ftu-fields.json')) as table_file:
@@ -131,7 +131,7 @@ def map(key, dims, value, context):
         # Convert ping time to date.    
         # If missing or invalid, ignore record. 
         try: 
-            ping_date = get_ping_date(data.get('pingTime'))
+            ping_date = ftu_formmatter.get_ping_date(data.get('pingTime'))
         except ValueError as e:
             write_condition(context, str(e))
             return
@@ -142,18 +142,21 @@ def map(key, dims, value, context):
         # Parse OS version string.
         # If missing or invalid, ignore record. 
         try:
-            os = get_os_version(data.get('deviceinfo.os'))
+            os = ftu_formmatter.get_os_version(data.get('deviceinfo.os'))
         except ValueError as e:
             write_condition(context, str(e))
             return
         vals['os'] = os
         
         # Look up geo-country.
-        vals['country'] = get_country(data.get('info').get('geoCountry'),
-            context.whitelist['country'], context.country_table)
+        vals['country'] = ftu_formmatter.get_country(
+            data.get('info').get('geoCountry'),
+            context.whitelist['country'], 
+            context.country_table)
         
         # Look up device name and reformat.
-        vals['device'] = get_device_name(data.get('deviceinfo.product_model'),
+        vals['device'] = ftu_formmatter.get_device_name(
+            data.get('deviceinfo.product_model'),
             context.whitelist['device'])
         
         # Add entries for "All" by expanding combinations of fields. 
