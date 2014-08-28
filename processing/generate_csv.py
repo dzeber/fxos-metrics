162e70baef7c04ac38a451ae3f3fffd762d5f5c9
Dave Zeber <dzeber@mozilla.com>
2014-08-28 10:45:43 -0500

    Updated processing script to be more robust in encoding output

--- processing/generate_csv.py
+++ processing/generate_csv.py
@@ -6,47 +6,74 @@
 import json
 import csv
 import sys
+import codecs
+import cStringIO
 
 job_output = sys.argv[1]
 csv_file = sys.argv[2]
+# The keys to write to the CSV output. 
 headers = ['pingdate', 'os', 'country', 'device', 'operator', 'count']
+# The keys that should not be encoded.
+not_encoded = ['count', 'pingdate']
+# The headings to use in the CSV, as expected by the display page script, 
+# corresponding to the keys in header by order.
 final_headers = ['date', 'os', 'country', 'device', 'operator', 'activations']
 
-
+# The complete set of parsed records. 
+# Each record is represented as a dict. 
+# In particular, there will be counts for every combination of fields 
+# with the value all.
 records = []
+# Error conditions encountered during the job. 
 conditions = {}
+# Counters recorded by the job. 
 counters = {}
 
 # Parse output file.
+
 for row in open(job_output):
     parsed_row = row.rstrip().rsplit('\t', 1)
-    key = json.loads(parsed_row[0])
+    vals = json.loads(parsed_row[0])
     n = int(parsed_row[1])
-    if 'condition' in key:
+    if 'condition' in vals:
         # Store mapping of condition counts. 
-        conditions[key['condition']] = n
-    elif 'counter' in key:
+        conditions[vals['condition']] = n
+    elif 'counter' in vals:
         # Store list of counters. 
-        if 'group' in key:
+        if 'group' in vals:
             # If the counter has a container group, 
             # add the counter in a subdict keyed by group name. 
-            group_name = key['group']
+            group_name = vals['group']
             if group_name not in counters:
                 counters[group_name] = {}
             grouplist = counters[group_name]
-            grouplist[key['counter']] = n
+            grouplist[vals['counter']] = n
         else:
-            counters[key['counter']] = n    
+            counters[vals['counter']] = n    
     else:
-        if key['pingdate'] != 'All':
-            key['count'] = n
-            records.append(key)
+        vals['count'] = n
+        records.append(vals)
 
-# Ouput CSV. 
+            
+# TODO: Find unique values of fields. 
+
+
+# Write CSV for main activations plot.
+
+# Custom row writing function to allow subsetting and additional formatting. 
+# Accepts the writer object and elements of records list.
+def write_row(csv_writer, row):
+    if row['pingdate'] != 'All':
+        # Reorder.
+        row = [row[key] for key in headers]
+        # Encode explicitly to try to avoid errors.
+        row = [unicode(r).encode('utf-8', 'replace') for r in row]
+        csv_writer.writerow(row)
+
 with open(csv_file, 'w') as outfile:
-    outfile.write(','.join(final_headers))
-    outfile.write('\n')
-    writer = csv.DictWriter(outfile, headers, extrasaction='ignore')
-    # writer.writeheader()
-    writer.writerows(records)
+    writer = csv.writer(outfile)
+    writer.writerow(final_headers)
+    for row in records:
+        write_row(writer, row)
+         
 
