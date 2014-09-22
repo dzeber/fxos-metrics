5f93c0d5a5b1c382a0af7140d0943c7d90b80cc9
Dave Zeber <dzeber@mozilla.com>
2014-09-22 18:37:35 +0100

    Added server-side submission date to raw dump

--- jobscripts/dump_all.py
+++ jobscripts/dump_all.py
@@ -2,5 +2,7 @@
 # A simple job to download all records.
 
 def map(key, dims, value, context):
+    # Add submission date to raw JSON.
+    value = value.rstrip('}')
+    value = value + ',"submissionDate":"' + dims.pop() + '"}'
     context.write(key, value)
-
