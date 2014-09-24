680db79e05e1c98100571d33836f00c88928ac59
Dave Zeber <dzeber@mozilla.com>
2014-09-24 17:58:20 +0100

    Stopped dump job from choking on bad dims

--- jobscripts/dump_all.py
+++ jobscripts/dump_all.py
@@ -1,8 +1,13 @@
 
 # A simple job to download all records.
 
+import mapred
+
 def map(key, dims, value, context):
-    # Add submission date to raw JSON.
-    value = value.rstrip('}')
-    value = value + ',"submissionDate":"' + dims.pop() + '"}'
+    # Add submission date to raw JSON, if available.
+    if len(dims) == 6:
+        value = value.rstrip('}')
+        value = value + ',"submissionDate":"' + dims[5] + '"}'
+    # else:
+        # write_condition(context, 'Irregular dims: ' + str(dims))
     context.write(key, value)
