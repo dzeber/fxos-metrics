7b5b52c83b349a8121db2b67b18477a71e09b721
Dave Zeber <dzeber@mozilla.com>
2015-05-19 13:29:13 -0500

    Cleaned up postprocessing code

--- postprocessing/ftu_dashboard_datasets.py
+++ postprocessing/ftu_dashboard_datasets.py
@@ -27,10 +27,6 @@ import utils.mapred as mapred
 import utils.ftu_formatter as ftu
 import utils.dump_schema as schema
 
-job_output = sys.argv[1]
-dashboard_csv = sys.argv[2]
-dump_csv = sys.argv[3]
-
 
 # Each datum will now be a tuple whose order is determined by 
 # schema.final_keys, rather than a dict.
@@ -38,26 +34,30 @@ dump_csv = sys.argv[3]
 field_index = dict(
     zip(schema.final_keys, range(0, len(schema.final_keys) - 1)))
 
+# The number of days before today the dashboard dataset should cover.
+dashboard_range = 180
+# The number of days before today the dump dataset should cover.
+dump_range = 90
 
 # Cutoff dates for inclusion in datasets.
 # No later than yesterday. 
 latest_date = (date.today() - timedelta(days = 1)).isoformat()
 # No earlier than 180 days ago. 
-earliest_date = (date.today() - timedelta(days = 180)).isoformat()
+earliest_date = (date.today() - timedelta(days = dashboard_range)).isoformat()
 # Cutoff date for inclusion in dump csv is 3 months before today.
-earliest_for_dump = (date.today() - timedelta(days = 90)).isoformat()
+earliest_for_dump = (date.today() - timedelta(days = dump_range)).isoformat()
 
 
 def encode_for_csv(val):
     """Encode a value as UTF-8."""
     return unicode(val).encode('utf-8', 'backslashreplace')
 
+
 def write_unicode_row(writer, row):
     """Write CSV row after encoding values explicity (to avoid errors)."""
     writer.writerow([encode_for_csv(v) for v in row])
 
-# Convert raw row to datum for inclusion in dashboard dataset. 
-# Accumulate counts.
+
 def accumulate_dashboard_row(dataset, raw_row):
     """Convert a raw datum to a row for the dashboard CSV, and add to dataset.
     
@@ -69,126 +69,96 @@ def accumulate_dashboard_row(dataset, raw_row):
     new_row = []
     # Extract relevant fields, and check them against lookup tables.
     # See dump_schema.py for list indices.
-    # Add date first. 
     new_row.append(raw_row[field_index['submissionDate']])
-    # OS
-    new_row.append(
-        # encode_for_csv(
-        ftu.summarize_os(raw_row[field_index['os']])
-        # )
-        )
-    # Country name.
-    new_row.append(
-        # encode_for_csv(
-        ftu.summarize_country(raw_row[field_index['country']])
-        # )
-        )
-    # Device name.
-    new_row.append(
-        # encode_for_csv(
-        ftu.summarize_device(raw_row[field_index['product_model']])
-        # )
-        )
-    # Operator name. 
-    new_row.append(
-        # encode_for_csv(
-        ftu.summarize_operator(
-            raw_row[field_index['icc.network']], 
-            raw_row[field_index['icc.name']], 
-            raw_row[field_index['network.network']], 
-            raw_row[field_index['network.name']])
-        # )
-        )
-    
+    new_row.append(ftu.summarize_os(raw_row[field_index['os']]))
+    new_row.append(ftu.summarize_country(raw_row[field_index['country']]))
+    new_row.append(ftu.summarize_device(raw_row[field_index['product_model']]))
+    new_row.append(ftu.summarize_operator(
+        raw_row[field_index['icc.network']], 
+        raw_row[field_index['icc.name']], 
+        raw_row[field_index['network.network']], 
+        raw_row[field_index['network.name']]
+    ))
     new_row = tuple(new_row)
+    # Add occurrence count from the original data.
     count = raw_row[-1]
-    
+    # Add new row to dashboard dataset, accumulating counts if necessary.
     if new_row not in dataset:
         dataset[new_row] = count
     else:
         dataset[new_row] = dataset[new_row] + count
 
 
-# Apply additional formatting to data records. 
-# This will mainly be value substitutions based on lookup tables,
-# and other 1-to-1 changes. 
-# def format_values(data_row):
-    # # Look up country code. 
-    # if data_row[field_index['country']] != '':
-        # country_name = ftu.lookup_country_code(r['country'])
-        # # If lookup fails, keep original geo code.
-        # if country_name is not None:
-            # data_row[field_index['country']] = country_name
+def main(job_output, dashboard_csv, dump_csv):
+    """Load map-reduce output, and write relevant subsets to CSVs.
     
-    # return data_row
-
-
-
-#-----------------------------
-
-# Parse in output file.
-data = mapred.parse_output_tuple(job_output)
-
-# Cutoff date for inclusion in dump csv is 3 months before today.
-# dump_cutoff_date = date.today() - timedelta(days = 90)
-# dump_cutoff_date = dump_cutoff_date.isoformat()
-
-# Accumulate subsets of data to be converted to CSV.
-# Dump rows will be stored as a list of row lists. 
-dump_rows = []
-# Dashboard rows will be stored as a mapping of value tuples to a count.
-dash_rows = {}
-
-for r in data['records']:
-    record_date = r[field_index['submissionDate']]
-    if record_date == '':
-        continue
-    if record_date > latest_date or record_date < earliest_date:
-        continue
+    The dump CSV includes full rows from the job output, but limited to a 
+    specific submission date range. The dashboard CSV is limited by submission 
+    date, but also restricted to certain columns.
     
-    # Apply formatting to be done outside MR job. 
-    # r = ftu.apply_post_formatting(r)
+    Input args are the path to the file containing the map-reduce job output,
+    stored using the tuple-based formatting defined in utils/mapred.py, and 
+    paths to the dashboard CSV and dump CSV to be written. 
+    """
+    data = mapred.parse_output_tuple(job_output)
     
-    # Add to dashboard data. 
-    accumulate_dashboard_row(dash_rows, r)
-    # Add to dump CSV if required. 
-    if record_date >= earliest_for_dump:
-        dump_rows.append(r)
+    # Accumulate subsets of data to be converted to CSV.
+    # Dump rows will be stored as a list of row lists. 
+    dump_rows = []
+    # Dashboard rows will be stored as a mapping of value tuples to a count.
+    dash_rows = {}
+    for r in data['records']:
+        record_date = r[field_index['submissionDate']]
+        if record_date == '':
+            continue
+        if record_date > latest_date or record_date < earliest_date:
+            continue
+        # Add to dashboard data. 
+        accumulate_dashboard_row(dash_rows, r)
+        # Add to dump CSV if required. 
+        if record_date >= earliest_for_dump:
+            dump_rows.append(r)
+    
+    # Write to output files.
+    headers = schema.dashboard_csv_headers
+    with open(dashboard_csv, 'w') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(headers)
+        for r in dash_rows:
+            next_row = list(r)
+            next_row.append(dash_rows[r])
+            write_unicode_row(writer, next_row)
+    
+    print('Wrote dashboard CSV: %s rows\n' % len(dash_rows))
+    
+    headers = schema.dump_csv_headers
+    with open(dump_csv, 'w') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(headers)
+        for r in dump_rows:
+            write_unicode_row(writer, r)
+    
+    print('Wrote dump CSV: %s rows\n' % len(dump_rows))
+    
+    # Output counters and diagnostics.
+    print('Counters:')
+    for name in data['counters']:
+        counter = data['counters'][name]
+        # If this is a group, print all subcounters.
+        if type(counter) is dict:
+            for cname in counter:
+                print(name + ' | ' + cname + ' :  ' + str(counter[cname]))
+        else:
+            print(name + ' :  ' + str(counter))
+    
+    print('\nError conditions:')
+    for name in data['conditions']:
+        print(name + ' :  ' + str(data['conditions'][name]))
 
 
-# Write to output files.
-headers = schema.dashboard_csv_headers
-with open(dashboard_csv, 'w') as outfile:
-    writer = csv.writer(outfile)
-    writer.writerow(headers)
-    for r in dash_rows:
-        next_row = list(r)
-        next_row.append(dash_rows[r])
-        write_unicode_row(writer, next_row)
-
-print('Wrote dashboard CSV: %s rows\n' % len(dash_rows))
-
-headers = schema.dump_csv_headers
-with open(dump_csv, 'w') as outfile:
-    writer = csv.writer(outfile)
-    writer.writerow(headers)
-    for r in dump_rows:
-        write_unicode_row(writer, r)
-
-print('Wrote dump CSV: %s rows\n' % len(dump_rows))
-
-# Output counters and diagnostics.
-print('Counters:')
-for name in data['counters']:
-    counter = data['counters'][name]
-    # If this is a group, print all subcounters.
-    if type(counter) is dict:
-        for cname in counter:
-            print(name + ' | ' + cname + ' :  ' + str(counter[cname]))
-    else:
-        print(name + ' :  ' + str(counter))
-
-print('\nError conditions:')
-for name in data['conditions']:
-    print(name + ' :  ' + str(data['conditions'][name]))
+if __name__ == "__main__":
+    job_output = sys.argv[1]
+    dashboard_csv = sys.argv[2]
+    dump_csv = sys.argv[3]
+    main(job_output, dashboard_csv, dump_csv)
 
