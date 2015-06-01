d25f379ce540e3ec9007d089e269c2c5b5624db3
Dave Zeber <dzeber@mozilla.com>
2015-06-01 17:44:26 +0100

    Updated adhoc runner to specify reason string

--- runjob.sh
+++ runjob.sh
@@ -10,6 +10,7 @@
 # Command-line option --until <date> gives end date (default is none).
 # Command-line option --ndays n gives the number of days.
 # --ndays can be combined with either of the other two. 
+# Option --reason can be used to specify the reason string (default is 'ftu').
 
 # Earliest date to consider is 2014-04-01.
 START_DATE_DEFAULT="20140401"
@@ -26,6 +27,7 @@ if [ $# -lt 2 ]; then
     echo "    --since <yyyy-mm-dd> : earliest date to include"
     echo "    --until <yyyy-mm-dd> : latest date to include"
     echo "    --ndays <n> : number of days to count"
+    echo "    --reason : reason string ('ftu' or 'appusage' for FxOS)"
     exit 1
 fi
 
@@ -49,6 +51,10 @@ while [ $# -gt 2 ]; do
         --filter)
             shift
             FILTER_FILE="$1"
+            ;;
+        --reason)
+            shift
+            REASON_STRING="$1"
             ;;
         *)
             echo "Invalid option: $1"
@@ -82,8 +88,8 @@ fi
 CURRENT_DIR=$(pwd)
 SRC_DIR=$(cd "`dirname "$0"`"; pwd)
 TELEMETRY_SERVER_DIR=$HOME/telemetry-server
-FILTER_TEMPLATE=$SRC_DIR/awsjobs/filters/$FILTER_TEMPLATE
 
+# Set up temporary storage.
 WORK_DIR=/mnt/telemetry/work
 DATA_CACHE=$WORK_DIR/cache
 
@@ -97,6 +103,11 @@ fi
 
 JOB_FILE=$SRC_DIR/awsjobs/$1
 OUTPUT_FILE=$CURRENT_DIR/$2
+# Path to the filter template that will be populated with custom values,
+# if required.
+FILTER_TEMPLATE=$SRC_DIR/awsjobs/filters/$FILTER_TEMPLATE
+# Path to filter file that will be used in the job
+# either a custom file or one generated from the template.
 FILTER=$SRC_DIR/awsjobs/filters/${FILTER_FILE:-_date_filter.json}  
 
 # Symlink utils dir into jobs dir so that telemetry-server job runner
@@ -105,15 +116,18 @@ if [ ! -e $SRC_DIR/awsjobs/utils ]; then
     ln -s $SRC_DIR/utils $SRC_DIR/awsjobs/utils
 fi
 
-# If filter file was not specified, set up filter with date range.
+# Generate a filter file from the template, if required.
 if [ -z "$FILTER_FILE" ]; then
-    # Use modified default filter.
+    cp $FILTER_TEMPLATE $FILTER
+    # Set the reason string.
+    sed -i'' "s/__REASON__/${REASON_STRING:-ftu}/" $FILTER
+    
+    # Set the date range.
     DATE_STRING="\"min\": \""$START_DATE"\""
     if [ -n "$END_DATE" ]; then
         DATE_STRING="$DATE_STRING, \"max\": \""$END_DATE"\""
     fi
-    
-    sed "s/__DATES__/$DATE_STRING/" $FILTER_TEMPLATE > $FILTER
+    sed -i'' "s/__DATES__/$DATE_STRING/" $FILTER
 fi
 
 echo "Running job $JOB_FILE with filter $FILTER"
