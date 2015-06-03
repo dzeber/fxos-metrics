23b32f0b0b4471c96f1831dd1a96c98e557371e0
Dave Zeber <dzeber@mozilla.com>
2015-06-03 13:20:22 +0100

    Commented out json-based mapred functions

--- utils/mapred.py
+++ utils/mapred.py
@@ -138,40 +138,40 @@ def parse_output_tuple(output_file):
 # These JSON-based outputs are not currently used in the fxos-metrics jobs.
 
 
-def dict_to_key(d):
-    """Convert a dict to a format that can be used as a map-reduce key.
+# def dict_to_key(d):
+    # """Convert a dict to a format that can be used as a map-reduce key.
     
-    Convert to JSON format (as a string). The items in the dict will be sorted
-    alphabetically by field_name to ensure that dicts containing the same keys 
-    are recorded as the same. 
-    """
-    return json.dumps(d, sort_keys=True)
+    # Convert to JSON format (as a string). The items in the dict will be sorted
+    # alphabetically by field_name to ensure that dicts containing the same keys 
+    # are recorded as the same. 
+    # """
+    # return json.dumps(d, sort_keys=True)
 
 
-def write_fieldvals(context, d): 
-    """Write a dict of field names mapping to values as a key mapping to 1,
-    in order to count occurrences. 
-    """
-    context.write(dict_to_key(d), 1)
+# def write_fieldvals(context, d): 
+    # """Write a dict of field names mapping to values as a key mapping to 1,
+    # in order to count occurrences. 
+    # """
+    # context.write(dict_to_key(d), 1)
 
 
-def increment_counter(context, name, group='', n=1):
-    """Increment a map-reduce counter by a specified value. 
+# def increment_counter(context, name, group='', n=1):
+    # """Increment a map-reduce counter by a specified value. 
     
-    The name can optionally be contained in a group. The key is of the form 
-    {'counter': name, 'group': group}, with group omitted if not specified. 
-    """
-    k = {'counter': name}
-    if len(group) > 0: 
-        k['group'] = group
-    context.write(dict_to_key(k), n)
+    # The name can optionally be contained in a group. The key is of the form 
+    # {'counter': name, 'group': group}, with group omitted if not specified. 
+    # """
+    # k = {'counter': name}
+    # if len(group) > 0: 
+        # k['group'] = group
+    # context.write(dict_to_key(k), n)
 
 
-def write_condition(context, condition):
-    """Count occurrences of end conditions. 
+# def write_condition(context, condition):
+    # """Count occurrences of end conditions. 
     
-    Key is of the form {'condition': condition}. 
-    """
-    context.write(dict_to_key({'condition': condition}), 1)
+    # Key is of the form {'condition': condition}. 
+    # """
+    # context.write(dict_to_key({'condition': condition}), 1)
 
 
