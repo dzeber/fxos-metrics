2522fa209fd4a1a0ec78865dde3984e7e7622c3f
Dave Zeber <dzeber@mozilla.com>
2015-06-23 12:14:26 -0700

    Added runner scripts for AU job

---
+++ dump_appusage.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Pass option '--nolog' to print all messages to stdout rather than log files. 
+# This is mainly for testing.
+LOG_TO_FILE=true
+if [ $# -gt 0 ] && [ "$1" = "--nolog" ]; then
+    LOG_TO_FILE=false
+fi
+
+
+# Dump all FxOS FTU records from the start date to the present.
+# START_DATE=`date +%Y%m%d -d "-9 months"`
+START_DATE=20150101
+
+CURRENT_DIR=$(pwd)
+SRC_DIR=$(cd "`dirname "$0"`"; pwd)
+TELEMETRY_SERVER_DIR=$HOME/telemetry-server
+
+# . settings.env
+
+OUTPUT_DIR="$CURRENT_DIR/output"
+OUTPUT_FILE="$OUTPUT_DIR/au_data.out"
+LOG_FILE="$OUTPUT_DIR/au_job.log"
+JOB_LOG="$OUTPUT_DIR/mapred.log"
+TARBALL=au_dump.tar.gz
+
+if [ ! -d "$OUTPUT_DIR" ]; then
+    mkdir "$OUTPUT_DIR"
+fi
+
+# Write output to log for debugging.
+$LOG_TO_FILE && exec > $LOG_FILE 2>&1
+
+echo "It is now `date`"
+echo "Preparing job..."
+
+WORK_DIR=$CURRENT_DIR/work
+DATA_DIR=$CURRENT_DIR/data
+
+if [ ! -d "$WORK_DIR" ]; then
+    mkdir "$WORK_DIR"
+fi
+
+if [ ! -d "$DATA_DIR" ]; then
+    mkdir "$DATA_DIR"
+fi
+
+JOB_FILE=$SRC_DIR/dump_format_appusage.py
+FILTER=$SRC_DIR/filter.json
+
+cp "$SRC_DIR/all_fxos_date.json" $FILTER
+# Set the reason string.
+sed -i'' "s/__REASON__/appusage/" $FILTER
+# Set the date range.
+DATE_STRING="\"min\": \""$START_DATE"\""
+# sed "s/__DATES__/$DATE_STRING/" $SRC_DIR/$FILTER_TEMPLATE > $FILTER
+sed -i'' "s/__DATES__/$DATE_STRING/" $FILTER
+
+echo "Job setup complete."
+echo "Running job." 
+
+cd "$TELEMETRY_SERVER_DIR"
+
+# Switch logging to separate file for job output.
+$LOG_TO_FILE && exec > $JOB_LOG 2>&1
+python -m mapreduce.job "$JOB_FILE" \
+   --input-filter "$FILTER" \
+   --num-mappers 16 \
+   --num-reducers 4 \
+   --work-dir "$WORK_DIR" \
+   --data-dir "$DATA_DIR" \
+   --output "$OUTPUT_FILE" \
+   --bucket "telemetry-published-v2" \
+   --verbose
+JOB_EXIT_CODE=$?
+
+# Back to main log file.
+$LOG_TO_FILE && exec > $LOG_FILE 2>&1
+echo "Mapreduce job exited with code: $JOB_EXIT_CODE"
+echo "It is now `date`"
+echo "Packaging output..."
+
+cd "$CURRENT_DIR"
+tar czf "$TARBALL" "`basename $OUTPUT_DIR`"
+rm -f $OUTPUT_DIR/*
+mv $TARBALL $OUTPUT_DIR
+
+echo "Done. Exiting..."
+
+exit 0
+
+
--- package_au_job.sh
+++ package_au_job.sh
@@ -1,7 +1,7 @@
-# Package necessary files to run job dumping raw AU data (JSON) on AWS cluster.
+# Package necessary files to run AU job on AWS cluster.
 # Creates a tarball in the current directory. 
 # The filename for the tarball can be passed as an optional argument. 
-# Default is "au_raw_dump-0.1.tar.gz".
+# Default is "au-dump-0.1.tar.gz".
 
 # Base dir for fxos-metrics code.
 BASE_DIR=$(cd "`dirname "$0"`"; pwd)
@@ -12,21 +12,22 @@ cd $BASE_DIR
 
 # Run AWS job from a flatter configuration. 
 # Add symlinks to flatten structure when archiving.
-ln -s $BASE_DIR/awsjobs/adhoc/dump_all.py $BASE_DIR/dump_all.py
+ln -s $BASE_DIR/awsjobs/dump_format_appusage.py $BASE_DIR/dump_format_appusage.py
 ln -s $BASE_DIR/awsjobs/filters/all_fxos_date.json $BASE_DIR/all_fxos_date.json
 
 # The utils dir needs to be inside the jobs dir to the job to run correctly.
 # Create a symlink to be followed when archiving.
 #ln -s $BASE_DIR/utils $BASE_DIR/awsjobs/utils
 
-tar cvfz "$TARGET_DIR/${1:-au_raw_dump-0.1.tar.gz}" -h \
-    dump_all.py \
+tar cvfz "$TARGET_DIR/${1:-au-dump-0.1.tar.gz}" -h \
+    dump_format_appusage.py \
     all_fxos_date.json \
     utils/*.py \
-    dump_all_au.sh
+    utils/lookup \
+    dump_appusage.sh
     
 # Remove symlink. 
-unlink $BASE_DIR/dump_all.py
+unlink $BASE_DIR/dump_format_appusage.py
 unlink $BASE_DIR/all_fxos_date.json
 
 exit 0
